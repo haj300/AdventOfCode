51f595a38f36af6ba672a07e2b988257ce28c917
haj300 <katja.lindeberg@gmail.com>
2020-12-10 10:31:22 +0100

    solution for day 5

--- day-5/src/main/java/Main.java
+++ day-5/src/main/java/Main.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -12,19 +11,67 @@ public class Main {
     private static final String INPUT = "input";
 
     public static void main(String[] args) {
-        final var input = readFile()
-                .collect(Collectors.joining("\n"))
-                .split("\n{2}");
+        final var input = readFile();
 
-        long count = Arrays.stream(input)
-                .map(Passport::new)
-                .filter(Passport::validate)
-                .count();
+      /* PART 1
+        var result = input.map(Main::getSeatID)
+                .max(Integer::compareTo)
+                .get();
+        System.out.println(result);
+       */
 
-        System.out.println(count);
+        var sortedIDs = readFile().map(Main::getSeatID)
+                .sorted(Integer::compareTo)
+                .collect(Collectors.toList());
+
+        for (int i = 0; i < sortedIDs.size() - 1; i++) {
+            if (sortedIDs.get(i + 1) - sortedIDs.get(i) != 1) {
+                System.out.println(sortedIDs.get(i) + 1);
+            }
+        }
     }
 
+    public static int getRow(String code) {
+        List<Integer> binaryString = getBinaryList(code, 'F');
+        return traverseBinaryCode(binaryString, 64);
+    }
 
+    private static List<Integer> getBinaryList(String code, char letter) {
+        return code.chars()
+                .map(current -> {
+                    if ((char) current == letter) {
+                        return 0;
+                    } else {
+                        return 1;
+                    }
+                })
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
+    public static int traverseBinaryCode(List<Integer> code, int binaryPosition) {
+        if (binaryPosition == 1) {
+            if (code.get(0) == 1) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+        if (code.get(0) == 1) {
+            return binaryPosition + traverseBinaryCode(code.subList(1, code.size()), binaryPosition / 2);
+        }
+        return traverseBinaryCode((code.subList(1, code.size())), binaryPosition / 2);
+    }
+
+    public static int getColumn(String code) {
+        List<Integer> binaryList = getBinaryList(code, 'L');
+        return traverseBinaryCode(binaryList, 4);
+    }
+
+    public static int getSeatID(String input) {
+
+        return getRow(input.substring(0, 7)) * 8 + getColumn(input.substring(7, 10));
+    }
 
     private static Stream<String> readFile() {
         try {
--- day-5/src/test/java/MainTest.java
+++ day-5/src/test/java/MainTest.java
@@ -4,15 +4,25 @@ import static org.junit.Assert.*;
 
 public class MainTest {
 
+
     @Test
     public void getRow() {
+        assertEquals(44, Main.getRow("FBFBBFF"));
+        assertEquals(70, Main.getRow("BFFFBBF"));
+        assertEquals(14, Main.getRow("FFFBBBF"));
+        assertEquals(102, Main.getRow("BBFFBBF"));
     }
 
     @Test
     public void getColumn() {
+        assertEquals(7, Main.getColumn("RRR"));
+        assertEquals(4, Main.getColumn("RLL"));
     }
 
     @Test
     public void getSeatID() {
+        assertEquals(567, Main.getSeatID("BFFFBBFRRR"));
+        assertEquals(119, Main.getSeatID("FFFBBBFRRR"));
+        assertEquals(820, Main.getSeatID("BBFFBBFRLL"));
     }
 }
